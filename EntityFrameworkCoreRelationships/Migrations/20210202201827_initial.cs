08f3c1c002e79c70d108be9e4fbf2b716b3590fd
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2021-02-02 15:50:56 -0500

    initial commit

---
+++ EntityFrameworkCoreRelationships/Migrations/20210202201827_initial.cs
@@ -0,0 +1,336 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkCoreRelationships.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hierarchicals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ParentId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hierarchicals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Hierarchicals_Hierarchicals_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Hierarchicals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ManyToManyLefts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManyToManyLefts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ManyToManyRelationships",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManyToManyRelationships", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ManyToManyRights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManyToManyRights", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NotRelateds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotRelateds", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OneToManyOwners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OneToManyOwners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OneToManys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OneToManys", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OneToOneOwners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Owned_Value = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OneToOneOwners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ManyToManyWithModeledLefts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RelationshipId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManyToManyWithModeledLefts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ManyToManyWithModeledLefts_ManyToManyRelationships_RelationshipId",
+                        column: x => x.RelationshipId,
+                        principalTable: "ManyToManyRelationships",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ManyToManyWithModeledRights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RelationshipId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManyToManyWithModeledRights", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ManyToManyWithModeledRights_ManyToManyRelationships_RelationshipId",
+                        column: x => x.RelationshipId,
+                        principalTable: "ManyToManyRelationships",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ManyToManyLeftManyToManyRight",
+                columns: table => new
+                {
+                    LeftsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RightsId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManyToManyLeftManyToManyRight", x => new { x.LeftsId, x.RightsId });
+                    table.ForeignKey(
+                        name: "FK_ManyToManyLeftManyToManyRight_ManyToManyLefts_LeftsId",
+                        column: x => x.LeftsId,
+                        principalTable: "ManyToManyLefts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ManyToManyLeftManyToManyRight_ManyToManyRights_RightsId",
+                        column: x => x.RightsId,
+                        principalTable: "ManyToManyRights",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OneToManyOwnedItem",
+                columns: table => new
+                {
+                    OneToManyOwnerId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OneToManyOwnedItem", x => new { x.OneToManyOwnerId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_OneToManyOwnedItem_OneToManyOwners_OneToManyOwnerId",
+                        column: x => x.OneToManyOwnerId,
+                        principalTable: "OneToManyOwners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OneToManyItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OneToManyId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OneToManyItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OneToManyItems_OneToManys_OneToManyId",
+                        column: x => x.OneToManyId,
+                        principalTable: "OneToManys",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OneToOneRights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    OneToOneLeftId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OneToOneRights", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OneToOneLefts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OneToOneRightId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RightId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OneToOneLefts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OneToOneLefts_OneToOneRights_RightId",
+                        column: x => x.RightId,
+                        principalTable: "OneToOneRights",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Hierarchicals_ParentId",
+                table: "Hierarchicals",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ManyToManyLeftManyToManyRight_RightsId",
+                table: "ManyToManyLeftManyToManyRight",
+                column: "RightsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ManyToManyWithModeledLefts_RelationshipId",
+                table: "ManyToManyWithModeledLefts",
+                column: "RelationshipId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ManyToManyWithModeledRights_RelationshipId",
+                table: "ManyToManyWithModeledRights",
+                column: "RelationshipId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OneToManyItems_OneToManyId",
+                table: "OneToManyItems",
+                column: "OneToManyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OneToOneLefts_RightId",
+                table: "OneToOneLefts",
+                column: "RightId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OneToOneRights_OneToOneLefts_Id",
+                table: "OneToOneRights",
+                column: "Id",
+                principalTable: "OneToOneLefts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OneToOneLefts_OneToOneRights_RightId",
+                table: "OneToOneLefts");
+
+            migrationBuilder.DropTable(
+                name: "Hierarchicals");
+
+            migrationBuilder.DropTable(
+                name: "ManyToManyLeftManyToManyRight");
+
+            migrationBuilder.DropTable(
+                name: "ManyToManyWithModeledLefts");
+
+            migrationBuilder.DropTable(
+                name: "ManyToManyWithModeledRights");
+
+            migrationBuilder.DropTable(
+                name: "NotRelateds");
+
+            migrationBuilder.DropTable(
+                name: "OneToManyItems");
+
+            migrationBuilder.DropTable(
+                name: "OneToManyOwnedItem");
+
+            migrationBuilder.DropTable(
+                name: "OneToOneOwners");
+
+            migrationBuilder.DropTable(
+                name: "ManyToManyLefts");
+
+            migrationBuilder.DropTable(
+                name: "ManyToManyRights");
+
+            migrationBuilder.DropTable(
+                name: "ManyToManyRelationships");
+
+            migrationBuilder.DropTable(
+                name: "OneToManys");
+
+            migrationBuilder.DropTable(
+                name: "OneToManyOwners");
+
+            migrationBuilder.DropTable(
+                name: "OneToOneRights");
+
+            migrationBuilder.DropTable(
+                name: "OneToOneLefts");
+        }
+    }
+}
---
+++ EntityFrameworkCoreRelationships/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCoreRelationships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var db = new Relationships();
+        }
+    }
+
+    public class Relationships : DbContext
+    {
+        public DbSet<NotRelated> NotRelateds { get; set; }
+        // 1 to 1 (bidirectional)
+        public DbSet<OneToOneLeft> OneToOneLefts { get; set; }
+        public DbSet<OneToOneRight> OneToOneRights { get; set; }
+        // 1 to 1 (owned)
+        public DbSet<OneToOneOwner> OneToOneOwners { get; set; }
+        // 1 to Many
+        public DbSet<OneToMany> OneToManys { get; set; }
+        public DbSet<OneToManyItem> OneToManyItems { get; set; }
+        // 1 to Many (owned)
+        public DbSet<OneToManyOwner> OneToManyOwners { get; set; }
+        // Many To Many (Transparent)
+        public DbSet<ManyToManyLeft> ManyToManyLefts { get; set; }
+        public DbSet<ManyToManyRight> ManyToManyRights { get; set; }
+        // Many To Many (Modeled Relationship)
+        public DbSet<ManyToManyWithModeledLeft> ManyToManyWithModeledLefts { get; set; }
+        public DbSet<ManyToManyWithModeledRight> ManyToManyWithModeledRights { get; set; }
+        public DbSet<ManyToManyRelationship> ManyToManyRelationships { get; set; }
+        // Hierarchical
+        public DbSet<Hierarchical> Hierarchicals { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=relationships.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // 1 to 1 (bidirectional)
+            modelBuilder.Entity<OneToOneLeft>()
+                .HasOne<OneToOneRight>()
+                .WithOne(r => r.Left)
+                .HasForeignKey<OneToOneRight>(r => r.Id);
+            
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class NotRelated
+    {
+        public int Id { get; set; }
+    }
+
+    public class OneToOneLeft
+    {
+        public int Id { get; set; }
+        public int OneToOneRightId { get; set; }
+        public OneToOneRight Right { get; set; }
+    }
+
+    public class OneToOneRight
+    {
+        public int Id { get; set; }
+        public int OneToOneLeftId { get; set; }
+        public OneToOneLeft Left { get; set; }
+    }
+
+    public class OneToOneOwner
+    {
+        public int Id { get; set; }
+        public OneToOneOwned Owned { get; set; } 
+    }
+
+    [Owned]
+    public class OneToOneOwned
+    {
+        public string Value { get; set; }
+    }
+
+    public class OneToMany
+    {
+        public int Id { get; set; }
+        public List<OneToManyItem> Items { get; set; } 
+    }
+
+    public class OneToManyItem
+    {
+        public int Id { get; set; }
+        public int OneToManyId { get; set; }
+        public OneToMany OneToMany { get; set; }
+    }
+    
+    public class OneToManyOwner
+    {
+        public int Id { get; set; }
+        public List<OneToManyOwnedItem> Items { get; set; }
+    }
+
+    [Owned]
+    public class OneToManyOwnedItem
+    {
+        public string Name { get; set; }
+    }
+
+    public class ManyToManyLeft
+    {
+        public int Id { get; set; }
+        public List<ManyToManyRight> Rights { get; set; }
+    }
+
+    public class ManyToManyRight
+    {
+        public int Id { get; set; }
+        public List<ManyToManyLeft> Lefts { get; set; }
+    }
+
+    public class ManyToManyWithModeledLeft
+    {
+        public int Id { get; set; }
+        public ManyToManyRelationship Relationship { get; set; }
+    }
+    
+    public class ManyToManyWithModeledRight
+    {
+        public int Id { get; set; }
+        public ManyToManyRelationship Relationship { get; set; }
+    }
+
+    public class ManyToManyRelationship
+    {
+        public int Id { get; set; }
+        public List<ManyToManyWithModeledLeft> Lefts { get; set; }
+        public List<ManyToManyWithModeledRight> Rights { get; set; }
+    }
+
+    public class Hierarchical
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public Hierarchical Parent { get; set; }
+        public List<Hierarchical> Children { get; set; }
+    }
+}
